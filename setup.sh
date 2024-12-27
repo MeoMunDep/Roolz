93309ef36fbc2ab042d6e6b04d90915e1965f0da
BlackCat <184516521+MeoMunDep@users.noreply.github.com>
2024-12-27 17:02:01 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -30,16 +30,13 @@ fi
 create_default_configs() {
     cat > configs.json << EOL
 {
-    "timeZone": "en-US",
-    "rotateProxy": false,
-    "skipInvalidProxy": false,
-    "proxyRotationInterval": 2,
-    "delayEachAccount": [5, 8],
-    "timeToRestartAllAccounts": 300,
-    "howManyAccountsRunInOneTime": 10,
-    "doTasks": true,
-    "playGames": true,
-    "referralCode": ""
+  "limit": 100,
+  "countdown": 300,
+  "country_time": "en-US",
+  "howManyGames": 1000,
+  "isDoTasks": true,
+  "referralCode": "871O187WS",
+  "delayEachAccount": [1, 81]
 }
 EOL
 }
@@ -55,7 +52,7 @@ check_configs() {
 while true; do
     clear
     echo "============================================================================"
-    echo "    name BOT SETUP AND RUN SCRIPT"
+    echo "    Roolz BOT SETUP AND RUN SCRIPT"
     echo "============================================================================"
     echo
     echo "Current directory: $(pwd)"
@@ -115,7 +112,7 @@ while true; do
             else
                 print_green "Using node_modules from current directory"
             fi
-            node bot
+            cd roolz && node bot
             read -p "Press Enter to continue..."
             ;;
         4)
